1b440f7e62414179b140c4e8b6ca13373ae2dce6
Sajjan <sajjan@Sajjan.local>
2023-01-03 14:18:22 +0545

    test

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("all")
         // Do any additional setup after loading the view.
     }
 
